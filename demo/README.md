03ac5745610ed00877a7d00ff5b511cb4b4f0c47
ecmadao <wlec@outlook.com>
2019-10-23 10:42:34 +0800

    update preview image and error message

--- demo/README.md
+++ demo/README.md
@@ -1,7 +1,7 @@
 
 ## 心知天气 V3 版插件自定义样式 DEMO
 
-**注：所有的 DEMO 需要用户在生成插件 token 之后，替换 index.html 文件中的 token 方可运行。**token 可在心知天气官网生成: https://seniverse.com/widget
+**注：所有的 DEMO 需要用户在生成插件 token 之后，替换 index.html 文件中的 token 方可运行。**token 可在心知天气官网生成: https://seniverse.com/widgetv3
 
 ### Run demo
 
@@ -13,3 +13,7 @@ $ npx http-server
 
 # 打开 http://localhost:8080/chinese/ 查看效果
 ```
+
+### 版权声明
+
+Demo 内图片和图标均来源自网络，仅做样例展示使用。
--- src/api/app.ts
+++ src/api/app.ts
@@ -18,7 +18,15 @@ const getConfig = (option: SwConfigOptions) => {
   const detected = getBrowserLanguage()
 
   if (token === '') {
-    alert('请在 "src/containers/Playground/index.tsx" 中填写 Token')
+    const error = '请填写 Token'
+    alert(`${error}。打开控制台查看更多提示信息`)
+    console.error(`错误❌：${error}`)
+    console.error(
+      '请在 https://www.seniverse.com/widgetv3 内生成插件所需的 token，并将它配置到 window.SeniverseWeatherWidget 方法中'
+    )
+    console.error(
+      '若本地开发调试或自行部署插件前端，建议将 token 配置到环境变量内，并保证能够传入到 App 组件内部'
+    )
   }
 
   return get(`${apiHost}/api/weather/${token}`, {
